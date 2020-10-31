815618d391b184066ec858dee073f48c96cdfd4f
Ap011y0n <47557365+Ap011y0n@users.noreply.github.com>
2020-10-31 20:35:00 +0100

    Camera speed reduced

--- NidhoggEngine/SourceCode/ModuleCamera3D.cpp
+++ NidhoggEngine/SourceCode/ModuleCamera3D.cpp
@@ -44,9 +44,9 @@ update_status ModuleCamera3D::Update(float dt)
 	// Now we can make this movememnt frame rate independant!
 
 	vec3 newPos(0,0,0);
-	float speed = 3.0f * dt;
+	float speed = 4.0f * dt;
 	if(App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT)
-		speed = 50.0f * dt;
+		speed = 8.0f * dt;
 
 	if(App->input->GetKey(SDL_SCANCODE_R) == KEY_REPEAT) newPos.y += speed;
 	if(App->input->GetKey(SDL_SCANCODE_T) == KEY_REPEAT) newPos.y -= speed;
