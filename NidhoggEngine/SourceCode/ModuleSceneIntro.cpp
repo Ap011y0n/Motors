c929808314b5e994728bc3335b11b90961ae31b1
Ap011y0n <47557365+Ap011y0n@users.noreply.github.com>
2020-10-31 13:18:45 +0100

    Scene intro loads 3 fbx

--- NidhoggEngine/SourceCode/ModuleSceneIntro.cpp
+++ NidhoggEngine/SourceCode/ModuleSceneIntro.cpp
@@ -47,49 +47,17 @@ bool ModuleSceneIntro::Start()
 	uint fileSize = 0;
 	fileSize = App->file_system->Load(file_path.c_str(), &buffer);
 	App->FBX->LoadFBX(buffer, fileSize);
-	//for (float i = -100; i < 100; i++)
-	//{
-	//	vec3 pos(i, 0.f, 0.f);
-	//	vec3 pos2(0.f, 0.f, i+1);
-	//	vec3 size(1.f, 1.f, 1.f);
-	//	Cube* cube = App->PrimManager->CreateCube(size, pos);
-	//	Cube* cube2 = App->PrimManager->CreateCube(size, pos2);
-	//}
-	//vec3 size(1.f, 1.f, 1.f);
-	//Pyramid* pyramid = App->PrimManager->CreatePyramid(size);
-	//pyramid->wire = false;
-	//cube->SetPos(pos.x, pos.y, pos.z);
-	//cube->SetRotation(45, pos);
-	//cube->Scale(1.f, 2.f, 1.f);
-	//cube->axis = true;
-	//cube->wire = true;
-	//vec3 pos(1.5f, 0.5f, -0.5f);
-	//PrimSphere* sphere = App->PrimManager->CreateSphere(0.5, 12, 24, pos);
-	//pos.Set(-1.3f, 0.f, 0.f);
-	//Cube* cube = App->PrimManager->CreateCube(size, pos);
 
-	//sphere->wire = true;
-	//pos.Set(3.5f, 0.5f, -0.5f);
+	file_path = "Assets/p1character.FBX";
+	fileSize = App->file_system->Load(file_path.c_str(), &buffer);
+	App->FBX->LoadFBX(buffer, fileSize);
 
-	//PrimSphere* sphere2 = App->PrimManager->CreateSphere(1, 100, 200, pos);
-	//sphere2->wire = true;
-	//pos.Set(3.f, 0.5f, -0.5f);
-	//PrimCylinder *cylinder = App->PrimManager->CreateCylinder(0.5f, 1.f, 30, pos);
+	file_path = "Assets/P1_PoldelaTorre.FBX";
+	fileSize = App->file_system->Load(file_path.c_str(), &buffer);
+	App->FBX->LoadFBX(buffer, fileSize);
 
 	vec4 coords(0, 1, 0, 0);
 	App->PrimManager->CreatePlane(coords);
-	//App->PrimManager->CreateLine(pos, size);
-	/*firstCube();
-	secondCube();*/
-	//vec3 origin(1, 1, 0);
-	//vec3 dest(0, 2, 2);
-
-	//App->PrimManager->CreateLine(origin, dest);
-
-
-	//glClearColor(0.0, 0.0, 0.0, 0.0);
-	//glShadeModel(GL_FLAT);
-	//glEnable(GL_DEPTH_TEST);
 
 
 	int i, j, c;
@@ -134,176 +102,9 @@ bool ModuleSceneIntro::CleanUp()
 update_status ModuleSceneIntro::Update(float dt)
 {
 
-	//glLineWidth(2.0f);
-	//glBegin(GL_TRIANGLES);
-	//glVertex3f(1.f, 1.f, 0.f);
-	//glVertex3f(0.f, 1.f, 0.f);
-	//glVertex3f(0.f, 0.f, 0.f);
-
-	//glVertex3f(0.f, 0.f, 0.f);
-	//glVertex3f(1.f, 0.f, 0.f);
-	//glVertex3f(1.f, 1.f, 0.f);
-
-	//glVertex3f(1.f, 1.f, 0.f);
-	//glVertex3f(1.f, 0.f, 0.f);
-	//glVertex3f(1.f, 0.f, 1.f);
-
-	//glVertex3f(1.f, 0.f, 1.f);
-	//glVertex3f(1.f, 1.f, 1.f);
-	//glVertex3f(1.f, 1.f, 0.f);
-
-	//glVertex3f(1.f, 1.f, 0.f);
-	//glVertex3f(1.f, 1.f, 1.f);
-	//glVertex3f(0.f, 1.f, 1.f);
-
-	//glVertex3f(0.f, 1.f, 1.f);
-	//glVertex3f(0.f, 1.f, 0.f);
-	//glVertex3f(1.f, 1.f, 0.f);
-
-	//glVertex3f(1.f, 1.f, 0.f);
-	//glVertex3f(1.f, 0.f, 0.f);
-	//glVertex3f(1.f, 0.f, 1.f);
-
-	//glVertex3f(1.f, 0.f, 1.f);
-	//glVertex3f(1.f, 1.f, 1.f);
-	//glVertex3f(1.f, 1.f, 0.f);
-
-	//glEnd();
-	//glLineWidth(1.0f);
-	/*
-	my_id = 0;
-	num_vertices = 24;
-	float vertices[72] =
-	{
-	1, 1, 0,
-	0, 1, 0,
-	0, 0, 0,
-	0, 0, 0,
-	1, 0, 0,
-	1, 1, 0,
-	1, 1, 0,
-	1, 0, 0,
-	1, 0, 1,
-	1, 0, 1,
-	1, 1, 1,
-	1, 1, 0,
-	1, 1, 0,
-	1, 1, 1,
-	0, 1, 1,
-	0, 1, 1,
-	0, 1, 0,
-	1, 1, 0,
-	1, 1, 0,
-	1, 0, 0,
-	1, 0, 1,
-	1, 0, 1,
-	1, 1, 1,
-	1, 1, 0,
-	};
-	glGenBuffers(1, (GLuint*)&(my_id));
-	glBindBuffer(GL_ARRAY_BUFFER, my_id);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * num_vertices * 3, vertices, GL_STATIC_DRAW);
-
-	glEnableClientState(GL_VERTEX_ARRAY);
-	glBindBuffer(GL_ARRAY_BUFFER, my_id);
-	glVertexPointer(3, GL_FLOAT, 0, NULL);
-	// … bind and use other buffers
-	glDrawArrays(GL_TRIANGLES, 0, num_vertices);
-	glDisableClientState(GL_VERTEX_ARRAY);
-	*/
-
-	
-
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	//glEnableClientState(GL_VERTEX_ARRAY);
-
-	//glBindBuffer(GL_ARRAY_BUFFER, my_vertex);
-	//glVertexPointer(3, GL_FLOAT, 0, NULL);
-	//
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, my_indices);
-
-	//glDrawElements(GL_TRIANGLES, num_indices, GL_UNSIGNED_INT, NULL);
-
-
-	//glBindBuffer(GL_ARRAY_BUFFER, my_vertex2);
-	//glVertexPointer(3, GL_FLOAT, 0, NULL);
-	//
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, my_indices2);
-
-	//glDrawElements(GL_TRIANGLES, num_indices2, GL_UNSIGNED_INT, NULL);
-
-	////*---------
-	//glDisableClientState(GL_VERTEX_ARRAY);
-	/*if (App->input->GetKey(SDL_SCANCODE_F2) == KEY_DOWN)
-	{
-		vec3 size(1.f, 1.f, 1.f);
-		Cube* cube = App->PrimManager->CreateCube(size);
-
-	}*/
-
-
-
-	/*glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glEnable(GL_TEXTURE_2D);
-	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
-	glBindTexture(GL_TEXTURE_2D, texName);
-	glBegin(GL_QUADS);
-	glTexCoord2f(0.0, 0.0); glVertex3f(-2.0, -1.0, 0.0);
-	glTexCoord2f(0.0, 1.0); glVertex3f(-2.0, 1.0, 0.0);
-	glTexCoord2f(1.0, 1.0); glVertex3f(0.0, 1.0, 0.0);
-	glTexCoord2f(1.0, 0.0); glVertex3f(0.0, -1.0, 0.0);
-	
-	glTexCoord2f(0.0, 0.0); glVertex3f(1.0, -1.0, 0.0);
-	glTexCoord2f(0.0, 1.0); glVertex3f(1.0, 1.0, 0.0);
-	glTexCoord2f(1.0, 1.0); glVertex3f(2.41421, 1.0, -1.41421);
-	glTexCoord2f(1.0, 0.0); glVertex3f(2.41421, -1.0, -1.41421);
-	glEnd();
-	glFlush();
-	glDisable(GL_TEXTURE_2D);*/
-
-	//ComponentTransform* transform = nullptr;
-	//for (int i = 0; i < gameObjects.size(); i++)
-	//{
-	//	for (int j = 0; j < gameObjects[i]->Components.size(); j++)
-	//	{
-	//		if (gameObjects[i]->Components[j]->type == ComponentType::TRANSFORM)
-	//		{
-	//			transform = (ComponentTransform*)gameObjects[i]->Components[j];
-	//		}
-	//	}
-	//	gameObjects[i]->Update(dt);
-	//}
-
 	UpdateGameObject(scene, dt);
 	SetDelete(scene);
 	DeleteGameObject(scene);
-	/*if (transform != nullptr)
-	{
-		if (App->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
-		{
-			transform->SetPos(transform->pos.x, transform->pos.y, transform->pos.z - 1 * dt);
-		}
-		if (App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
-		{
-			transform->SetPos(transform->pos.x, transform->pos.y, transform->pos.z + 1 * dt);
-		}
-		if (App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
-		{
-			transform->SetPos(transform->pos.x - 1 * dt, transform->pos.y, transform->pos.z);
-		}
-		if (App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
-		{
-			transform->SetPos(transform->pos.x + 1 * dt, transform->pos.y, transform->pos.z);
-		}
-		if (App->input->GetKey(SDL_SCANCODE_RSHIFT) == KEY_REPEAT)
-		{
-			transform->SetPos(transform->pos.x, transform->pos.y - 1 * dt, transform->pos.z);
-		}
-		if (App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT)
-		{
-			transform->SetPos(transform->pos.x, transform->pos.y + 1 * dt, transform->pos.z);
-		}
-	}*/
 	
 	return UPDATE_CONTINUE;
 }
